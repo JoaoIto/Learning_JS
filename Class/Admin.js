e69ab48a3c46c0b21650a097bb4b19a2429be668
João <joaovictorpfr@gmail.com>
2022-08-25 21:15:19 -0300

    Add code

--- Class/Admin.js
+++ Class/Admin.js
@@ -1,7 +1,11 @@
-import User from "./User";
+import User from "./User.js";
 
 class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = true){
-        super(nome, email, nascimento, role, ativo)
-    }
-}
+        super(nome, email, nascimento, role, ativo);
+    };
+};
+
+const novoAdmin = new Admin("Mly", "mly@gmail.com", "16/01/2005");
+console.log(novoAdmin);
+console.log(novoAdmin.exibirInfos());
