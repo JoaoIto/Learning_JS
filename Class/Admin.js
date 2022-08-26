fd7aa60b2d22f9d912567ebf5432143fff9b8cd9
João <joaovictorpfr@gmail.com>
2022-08-25 21:16:53 -0300

    Add code

--- Class/Admin.js
+++ Class/Admin.js
@@ -2,6 +2,9 @@ import User from "./User.js";
 
 class Admin extends User {
     constructor(nome, email, nascimento, role = "admin", ativo = true){
+        /* Herdando a partir de um super, da classe de User, (classe mãe)
+        Eles são herdados tantos os atributos e seus métodos, que são já
+        especificados no próprio super...*/
         super(nome, email, nascimento, role, ativo);
     };
 };
