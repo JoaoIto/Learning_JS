9833c9235787dd04b20f26248c176bcb22aaad85
João <joaovictorpfr@gmail.com>
2022-08-25 21:06:27 -0300

    Add code

--- Class/User.js
+++ Class/User.js
@@ -1,17 +1,17 @@
 export default class User {
-    constructor(nome, email, nascimento, role, ativo = true){
-        /* Atributos definidos nos parãmetros sendo valorizados */
-        this.nome = nome;
-        this.email = email;
-        this.nascimento = nascimento;
-        this.role = role || "estudante";
-        this.ativo = ativo;
-    };
-    /* Método de exibição de informações sendo instanciado */
-    exibirInfos(){
-        return `${this.nome}, ${this.email}`;
-    };
-};
+  constructor(nome, email, nascimento, role, ativo = true) {
+    /* Atributos definidos nos parãmetros sendo valorizados */
+    this.nome = nome;
+    this.email = email;
+    this.nascimento = nascimento;
+    this.role = role || "estudante";
+    this.ativo = ativo;
+  }
+  /* Método de exibição de informações sendo instanciado */
+  exibirInfos() {
+    return `${this.nome}, ${this.email}`;
+  }
+}
 
 const novoUser = new User("Joao", "joao@gmail.com", "23/08/2005");
 console.log(novoUser);
